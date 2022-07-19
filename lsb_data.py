c886ad6b8646be6e156ad662c323ba2ab696c99e
Mariah Jo Corso <mariahjocorso@gmail.com>
2022-07-19 11:28:59 -0400

    adding more data options

--- lsb_data.py
+++ lsb_data.py
@@ -1,19 +1,34 @@
 # modules for reading the data
 
+import os
 import pandas as pd
 import numpy as num
 import seaborn as sns
 
 # funtion to read the data
-Seeds_df = pd.read_csv(
-    'assests\Seedbank-Python-Sheet1.csv')
-Seeds_df.head()
-print(Seeds_df)
+# Seeds_df = pd.read_csv(
+#   'assests\Seedbank-Python-Sheet1.csv')
+# Seeds_df.head()
+# print(Seeds_df)
 
 # changing the data to be read as a list
 
+# Seeds_df = pd.read_csv(
+#  'assests\Seedbank-Python-Sheet1.csv', delimiter=',')
+# list_of_rows = [list(row) for row in Seeds_df.values]
+# list_of_rows.insert(0, Seeds_df.columns.to_list())
+# print(list_of_rows)
+
+# Changing function to only print out list of Seed Names
+
+# Seeds_df = pd.read_csv(
+#    'assests\Seedbank-Python-Sheet1.csv')
+# Seeds_type = Seeds_df['Type']
+# print(Seeds_type)
+
+# printing as a dictionary (can this be used for what I want to do???)
+
 Seeds_df = pd.read_csv(
-    'assests\Seedbank-Python-Sheet1.csv', delimiter=',')
-list_of_rows = [list(row) for row in Seeds_df.values]
-list_of_rows.insert(0, Seeds_df.columns.to_list())
-print(list_of_rows)
+    'assests\Seedbank-Python-Sheet1.csv', usecols=['Type', 'Sun Requirements'])
+Seeds_df.to_dict(orient='dict')
+print(Seeds_df)
