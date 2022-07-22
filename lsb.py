61d2f08e6484d6ba8c5b4b97608e04ec35b714fa
Mariah Jo Corso <mariahjocorso@gmail.com>
2022-07-21 21:29:17 -0400

    added a graph and organized said graph using Seaborn

--- lsb.py
+++ lsb.py
@@ -1,6 +1,8 @@
 # First attempt at making a function
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Prints list of the seeds
 # NEED TO ADD STRING PRINT TO DESCRIBE LIST
@@ -10,23 +12,32 @@ print(Seeds_df)
 
 Seeds_df1 = pd.read_csv(
     'assests\Seedbank-Python-Sheet1.csv')
-#Seeds_type = Seeds_df1['Type']
-# print(Seeds_type)
-print(Seeds_df1.columns.tolist())
+Seeds_type = Seeds_df1['Type']
+print(Seeds_type)
+# print(Seeds_df1.columns.tolist())
 
 Seeds_type = Seeds_df1['Type']
 harv_sea = Seeds_df1['Season']
 sun = Seeds_df1['Sun Requirements']
-#day = Seeds_df1['Days Until Yeild']
+day = Seeds_df1['Average Days to Maturity ']
 
 
 #select = input('Enter Seed Name: ')
-# if select == -1:
-#    print("We don't have that seed (yet!)")
+# if select == (index_col=[0, 20]):
+#   print("We don't have that seed (yet!)")
 # else:
-#    print(
-#        "Seed's specifications are: \n 'Harvest Season: ', [1] , \n Sun Requiremnts: ', sun, '\n Average Days to Maturity: ', 'days'")
+#   print(
+#       "Seed's specifications are: \n 'Harvest Season: ', [1] , \n Sun Requiremnts: ', sun, '\n Average Days to Maturity: ', 'days'")
 
 #   exit_choice = input('Do you want to exit [Yes|No]: ')
 #   if exit_choice == 'No' or exit_choice == 'no':
 # break
+
+
+sns.barplot(x='Average Days to Maturity ',
+            y='Type',
+            data=Seeds_df1,
+            order=Seeds_df1.sort_values('Average Days to Maturity ').Type)
+
+# Show the plot
+plt.show()
