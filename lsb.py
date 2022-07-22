fbf0eb21541b2222a03816e59a696d4c293171bf
Mariah Jo Corso <mariahjocorso@gmail.com>
2022-07-21 23:09:21 -0400

    main function still non-working/ edited bar plot to be organdized in asending order

--- lsb.py
+++ lsb.py
@@ -5,39 +5,43 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Prints list of the seeds
-# NEED TO ADD STRING PRINT TO DESCRIBE LIST
+
+Welcome_message = "Here is a list of Louisville Seedbank's Top 20 most popular Seeds!"
+print(Welcome_message)
+
 path = 'assests\Seedbank-Python-Sheet1.csv'
 Seeds_df = os.path.join(path, 'assets', 'Seedbank-Python-Sheet1.csv')
-print(Seeds_df)
+# print(Seeds_df)
 
 Seeds_df1 = pd.read_csv(
     'assests\Seedbank-Python-Sheet1.csv')
 Seeds_type = Seeds_df1['Type']
-print(Seeds_type)
+print(Seeds_type.tolist())
 # print(Seeds_df1.columns.tolist())
 
 Seeds_type = Seeds_df1['Type']
 harv_sea = Seeds_df1['Season']
 sun = Seeds_df1['Sun Requirements']
-day = Seeds_df1['Average Days to Maturity ']
+days = Seeds_df1['Average Days to Maturity ']
 
 
-#select = input('Enter Seed Name: ')
-# if select == (index_col=[0, 20]):
-#   print("We don't have that seed (yet!)")
+get_requirements = input('Enter Seed Name: ')
+
+if input == Seeds_type():
+    print("We don't have that seed (yet!)")
 # else:
-#   print(
+#     print(
 #       "Seed's specifications are: \n 'Harvest Season: ', [1] , \n Sun Requiremnts: ', sun, '\n Average Days to Maturity: ', 'days'")
 
 #   exit_choice = input('Do you want to exit [Yes|No]: ')
 #   if exit_choice == 'No' or exit_choice == 'no':
 # break
 
-
-sns.barplot(x='Average Days to Maturity ',
-            y='Type',
-            data=Seeds_df1,
-            order=Seeds_df1.sort_values('Average Days to Maturity ').Type)
+# PRINTS BARPLOT WORKS
+# sns.barplot(x='Average Days to Maturity ',
+#            y='Type',
+#            data=Seeds_df1,
+#            order=Seeds_df1.sort_values('Average Days to Maturity ').Type)
 
 # Show the plot
-plt.show()
+# plt.show()
